fb5e998cbcfd07df0fbfa8d2769010a06f5d7096
lucky <166947747+luckyxjx@users.noreply.github.com>
2025-09-14 21:09:57 +0530

    ..errors .. eslint

--- src/app/api/compliment/route.ts
+++ src/app/api/compliment/route.ts
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unescaped-entities */
+
 import { NextRequest } from 'next/server';
 import { APIResponseUtil } from '@/utils/api-response';
 import { Redis } from '@upstash/redis';
